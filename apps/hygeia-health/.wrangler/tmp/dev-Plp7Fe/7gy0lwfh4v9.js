a909d13fa0133affc2f0d2d652ebdda820fbf84e
crypto0627 <jake0627a1@gmail.com>
2025-05-06 10:33:38 +0800

    fix: wrangler bundle

--- apps/hygeia-health/.wrangler/tmp/dev-Plp7Fe/7gy0lwfh4v9.js
+++ apps/hygeia-health/.wrangler/tmp/dev-Plp7Fe/7gy0lwfh4v9.js
@@ -3211,7 +3211,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env3, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-1vxsmK/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-kPOIRe/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -3243,7 +3243,7 @@ function __facade_invoke__(request, env3, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-1vxsmK/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-kPOIRe/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
